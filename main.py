574a1d846ea941c53b106583f6d59fe617a639eb
bahati <bulozealfani@gmail.com>
2023-03-02 16:45:55 -0600

    added definition and words

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
-
+import pandas as pd
 app = Flask(__name__)
 
+
+filename = 'dictionary.csv'
+df = pd.read_csv(filename)
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -9,9 +12,9 @@ def home():
 
 @app.route("/api/v1/<word>/")
 def about(word):
-    detail = word.upper()
+    detail = df.loc[df['word'] == word]['definition'].squeeze()
     return {"Description": detail,
-            "Word": word}
+                "Word": word}
 
 
 if __name__ == "__main__":
